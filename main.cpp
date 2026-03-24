924af6f891dd7577a5da1b43230d7a9d3de90226
Rollin Gilman <rdgilman@my.canyons.edu>
2026-03-24 12:51:01 -0700

    A43 minimum of three integers

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int num1, num2, num3;
@@ -10,6 +11,19 @@ int main()
 
     // TODO: Find the smallest value among three values
 
+    min = num1;
+
+    if (num2 < min)
+    {
+        min = num2;
+    }
+
+    if (num3 < min)
+    {
+        min = num3;
+    }
     // Use the following statements to print output
     cout << "The smallest number is  " << min << endl;
+
+    return 0;
 }
